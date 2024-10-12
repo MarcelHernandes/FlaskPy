4d37128f60e6814762fb45260cc31752632828a3
MARCEL HERNANDES SANT ANNA <marcel.hsanna@senacsp.edu.br>
2024-10-11 22:06:45 -0300

    Segundo Commit

--- app/routes.py
+++ app/routes.py
@@ -30,3 +30,43 @@ def cadastrarUsuario():
         return 'Cadastrado com sucesso!'
     except Exception as e:
         return f'Ocorreu um erro\n\n {e}'
+
+@app.route('/listar')
+def listarTudo():
+    try:
+        requisicao = requests.get(f'{link}/cadastrar/.json') #solicitação dos dados
+        dicionario = requisicao.json()
+        return dicionario
+    except Exception as e:
+        return f'Algo deu errado!! \n\n{e}'
+
+@app.route('/listarIndividual')
+def listarIndividual():
+    try:
+        requisicao = requests.get(f'{link}/cadastrar/.json') #solicitar os dados
+        dicionario = requisicao.json()
+        idCadastro = "" #Armazenar o ID individual de cada um
+        for codigo in dicionario:
+            chave = dicionario[codigo]['cpf']
+            if chave == '123':
+                idCadastro = codigo
+            return idCadastro
+    except Exception as e:
+        return f'Algo deu errado!! \n\n{e}'
+
+@app.route('/atualizar')
+def atualizar():
+    try:
+        dados = {"nome":"Lernandes"},{"telefone":"1000"},{"endereco":"Rua z..."}#Dados novos
+        requisicao = requests.patch(f'{link}/cadastrar/-O8Juk2L87yfj9TcdkLi/.json', data=json.dumps(dados))
+        return "Atualizado com Sucesso!!"
+    except Exception as e:
+        return f'Algo deu errado\n\n {e}'
+
+@app.route('/excluir')
+def excluir():
+    try:
+        requisicao = requests.delete(f'{link}/cadastrar/-O8y-ssQWkuQta4iZaqZ/.json')
+        return "Excluído com sucesso!!"
+    except Exception as e:
+        return f"Algo deu errado \n\n {e}"
